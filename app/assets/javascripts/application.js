36df59c2930ca87d698afe6c40f209fe18739611
Simon Dunn <simondunn90@gmail.com>
2024-01-16 10:27:33 +0000

    single business and multi business message tabs updated

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,13 +19,16 @@ window.GOVUKPrototypeKit.documentReady(() => {
     
     // Dynamic styling of read/unread tag
     function updateUnreadMessagesCount() {
-      // line 23 updates the message count and applied the correct styling when allUnreadMessages.length === 0, but the wrong message count is shown on the multiple businesses page
-      const allMessagesTab = document.querySelector('.govuk-tabs__list-item-ffd a[href="#all-messages"]');
-      // line 24 restores the All messages tab to a message count of 702, but the message count no longer updates & the correct styling isn't applied when allUnreadMessages.length === 0
-      // const allMessagesTab = document.querySelector('.govuk-tabs__list-item-ffd a[href="#all-messages"], .govuk-tabs__list-item-ffd a[href=#overview]');
+      const allMessagesSb = document.querySelector('.single-business');
+      const allMessagesMb = document.querySelector('.multi-business');
       const allUnreadMessages = document.querySelectorAll('.govuk-details-message-unread');
       const allReadMessages = document.querySelectorAll('.govuk-details-message');
-      allMessagesTab.innerHTML = allUnreadMessages.length === 0 ? `All messages <span class="read-message">${allReadMessages.length} read</span>` : `All messages <span class="unread-message">${allUnreadMessages.length} unread</span>`
+
+      if (allMessagesSb) {
+        allMessagesSb.innerHTML = allUnreadMessages.length === 0 ? `All messages <span class="read-message">${allReadMessages.length} read</span>` : `All messages <span class="unread-message">${allUnreadMessages.length} unread</span>`
+      } else {
+        allMessagesMb.innerHTML = `All messages <span class="unread-message">${699 + allUnreadMessages.length} unread</span>`
+      }
     }
 
     updateUnreadMessagesCount();
