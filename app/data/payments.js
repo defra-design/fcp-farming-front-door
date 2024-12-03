8eb5bea94260ecb79159ff8c64106e64eb50f522
Dave Haigh <davehaigh@outlook.com>
2024-12-03 14:29:45 +0000

    v6 blue banner and payments data

--- app/data/payments.js
+++ app/data/payments.js
@@ -1,79 +1,95 @@
 module.exports = [
     {
-        "scheme": "sfi",
-        "action": "Assess soil, test soil organic matter and produce a soil management plan",
-        "code": "SAM1",
-        "amount": 139.75,
-        "date": "5 July 2024"
+        "id": 1,
+        "reference": "DEF001",
+        "amount": 706,
+        "date": "5 July 2024",
+        "period": "1 April to 30 June 2024",
+        "items": [
+            {
+                "description": "Assess soil, test soil organic matter and produce a soil management plan",
+                "code": "SAM1",
+                "scheme": "SFI 23",
+                "amount": 139.75
+            },
+            {
+                "description": "Assess and record hedgerow condition",
+                "code": "HRW1",
+                "scheme": "SFI 23",
+                "amount": 94.87,
+            },
+            {
+                "description": "Manage hedgerows",
+                "code": "HRW2",
+                "scheme": "SFI 23",
+                "amount": 316.25,
+            },
+            {
+                "description": "Assess integrated pest management and produce a plan",
+                "code": "IPM1",
+                "scheme": "SFI 23",
+                "amount": 247.25,
+            },
+            {
+                "description": "Assess nutrient management and produce a review report",
+                "code": "NUM1",
+                "scheme": "SFI 23",
+                "amount": 147.25,
+            }
+        ]
     },
     {
-        "scheme": "sfi",
-        "action": "Assess and record hedgerow condition",
-        "code": "HRW1",
-        "amount": 94.87,
-        "date": "5 July 2024"
-    },
-    {
-        "scheme": "sfi",
-        "action": "Manage hedgerows",
-        "code": "HRW2",
-        "amount": 316.25,
-        "date": "5 July 2024"
-    },
-    {
-        "scheme": "sfi",
-        "action": "Assess integratd pest management and produce a plan",
-        "code": "IPM1",
-        "amount": 247.25,
-        "date": "5 July 2024"
-    },
-    {
-        "scheme": "sfi",
-        "action": "Assess nutrient management and produce a review report",
-        "code": "NUM1",
-        "amount": 147.25,
-        "date": "5 July 2024"
-    },
-    {
-        "scheme": "vets visit",
-        "action": "vet visit claim",
-        "code": "VV",
+        "id": 2,
+        "reference": "DEF002",
         "amount": 1500,
-        "date": "15 August 2024"
+        "date": "15 August 2024",
+        "period": "1 April to 30 November 2024",
+        "items": [
+            {
+                "description": "Animal health and welfare claim (July 2024)",
+                "code": "DEF003",
+                "scheme": "AHWP",
+                "amount": 1500,
+            }
+        ]
     },
     {
-        "scheme": "sfi",
-        "action": "Assess soil, test soil organic matter and produce a soil management plan",
-        "code": "SAM1",
-        "amount": 139.75,
-        "date": "5 October 2024"
-    },
-    {
-        "scheme": "sfi",
-        "action": "Assess and record hedgerow condition",
-        "code": "HRW1",
-        "amount": 94.87,
-        "date": "5 October 2024"
-    },
-    {
-        "scheme": "sfi",
-        "action": "Manage hedgerows",
-        "code": "HRW2",
-        "amount": 316.25,
-        "date": "5 October 2024"
-    },
-    {
-        "scheme": "sfi",
-        "action": "Assess integratd pest management and produce a plan",
-        "code": "IPM1",
-        "amount": 247.25,
-        "date": "5 October 2024"
-    },
-    {
-        "scheme": "sfi",
-        "action": "Assess nutrient management and produce a review report",
-        "code": "NUM1",
-        "amount": 147.25,
-        "date": "5 October 2024"
+        "id": 3,
+        "reference": "DEF003",
+        "amount": 706,
+        "date": "5 October 2024",
+        "period": "July to 30 September 2024",
+        "items": [
+            {
+                "description": "Assess soil, test soil organic matter and produce a soil management plan",
+                "code": "SAM1",
+                "scheme": "SFI 23",
+                "amount": 139.75
+            },
+            {
+                "description": "Assess and record hedgerow condition",
+                "code": "HRW1",
+                "scheme": "SFI 23",
+                "amount": 94.87,
+            },
+            {
+                "description": "Manage hedgerows",
+                "code": "HRW2",
+                "scheme": "SFI 23",
+                "amount": 316.25,
+            },
+            {
+                "description": "Assess integrated pest management and produce a plan",
+                "code": "IPM1",
+                "scheme": "SFI 23",
+                "amount": 247.25,
+            },
+            {
+                "description": "Assess nutrient management and produce a review report",
+                "code": "NUM1",
+                "scheme": "SFI 23",
+                "amount": 147.25,
+            }
+        ]
     }
 ]
