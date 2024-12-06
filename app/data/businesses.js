4ed99d5aa6e5bd9223c2ab7b57d45059f58ec0f8
Dave Haigh <davehaigh@outlook.com>
2024-12-06 15:07:11 +0000

    v6 removed some distracting business names

--- app/data/businesses.js
+++ app/data/businesses.js
@@ -1,162 +1,132 @@
 module.exports = [
-    {
-      "id": 1,
-      "name": "Agile Farms Ltd",
-      "sbiNumber": 123456789
-    },
-    {
-      "id": 2,
-      "name": "AJ COTTEY",
-      "sbiNumber": 369888333
-    },
-    {
-      "id": 3,
-      "name": "Avery Acres",
-      "sbiNumber": 744566000
-    },
-    {
-      "id": 4,
-      "name": "Beets by Dre",
-      "sbiNumber": 332219845
-    },
-    {
-      "id": 5,
-      "name": "Croft Industries",
-      "sbiNumber": 755211109
-    },
-    {
-      "id": 6,
-      "name": "D M Jones & Partners",
-      "sbiNumber": 202736410
-    },
-    {
-      "id": 7,
-      "name": "Dickson Bros",
-      "sbiNumber": 9944447
-    },
-    {
-      "id": 8,
-      "name": "Dolly Parsnip Ltd",
-      "sbiNumber": 860763222
-    },
-    {
-      "id": 9,
-      "name": "Estate of Lady Higginbottom",
-      "sbiNumber": 865531123
-    },
-    {
-      "id": 10,
-      "name": "Fenton Hectares",
-      "sbiNumber": 96445221
-    },
-    {
-      "id": 11,
-      "name": "FOSTER & SON",
-      "sbiNumber": 221097732
-    },
-    {
-      "id": 12,
-      "name": "Grays Poultry Ltd",
-      "sbiNumber": 107756778
-    },
-    {
-      "id": 13,
-      "name": "Hollybush Holdings",
-      "sbiNumber": 122109876
-    },
-    {
-      "id": 14,
-      "name": "Home Farm",
-      "sbiNumber": 644322221
-    },
-    {
-      "id": 15,
-      "name": "Inroad Developments Ltd",
-      "sbiNumber": 2001421786
-    },
-    {
-      "id": 16,
-      "name": "Jehan and Wearn Ltd",
-      "sbiNumber": 344211567
-    },
-    {
-      "id": 17,
-      "name": "Kelce Farm",
-      "sbiNumber": 889045441
-    },
-    {
-      "id": 18,
-      "name": "Marley & Daughters",
-      "sbiNumber": 122176668
-    },
-    {
-      "id": 19,
-      "name": "Mitchell Bros",
-      "sbiNumber": 89912332
-    },
-    {
-      "id": 20,
-      "name": "Nigel Thornberry",
-      "sbiNumber": 231109776
-    },
-    {
-      "id": 21,
-      "name": "Oakley Farm",
-      "sbiNumber": 433232111
-    },
-    {
-      "id": 22,
-      "name": "Potter Holdings Ltd",
-      "sbiNumber": 500093456
-    },
-    {
-      "id": 23,
-      "name": "Prime Cuts Farm",
-      "sbiNumber": 500093456
-    },
-    {
-      "id": 24,
-      "name": "RJ Whitfield",
-      "sbiNumber": 433876541
-    },
-    {
-      "id": 25,
-      "name": "Ronnie's Farm",
-      "sbiNumber": 761132198
-    },
-    {
-      "id": 26,
-      "name": "Russer & Mona",
-      "sbiNumber": 121166709
-    },
-    {
-      "id": 27,
-      "name": "Sharon's Farm",
-      "sbiNumber": 489910429
-    },
-    {
-      "id": 28,
-      "name": "Shaw & Carter Ltd",
-      "sbiNumber": 122887431
-    },
-    {
-      "id": 29,
-      "name": "Sunflower Charity",
-      "sbiNumber": 987677432
-    },
-    {
-      "id": 30,
-      "name": "Todmorden Shrimp Farm",
-      "sbiNumber": 121150991
-    },
-    {
-      "id": 31,
-      "name": "Valley Farm",
-      "sbiNumber": 192846125
-    },
-    {
-      "id": 32,
-      "name": "Wright Sisters",
-      "sbiNumber": 123335090
-    }
-   ]
+  {
+    "id": 1,
+    "name": "Agile Farms Ltd",
+    "sbiNumber": 123456789
+  },
+  {
+    "id": 2,
+    "name": "AJ COTTEY",
+    "sbiNumber": 369888333
+  },
+  {
+    "id": 3,
+    "name": "Avery Farms",
+    "sbiNumber": 744566000
+  },
+  {
+    "id": 4,
+    "name": "Croft Industries",
+    "sbiNumber": 755211109
+  },
+  {
+    "id": 5,
+    "name": "D M Jones & Partners",
+    "sbiNumber": 202736410
+  },
+  {
+    "id": 6,
+    "name": "FOSTER & SON",
+    "sbiNumber": 221097732
+  },
+  {
+    "id": 7,
+    "name": "Grays Poultry Ltd",
+    "sbiNumber": 107756778
+  },
+  {
+    "id": 8,
+    "name": "Hollybush Holdings",
+    "sbiNumber": 122109876
+  },
+  {
+    "id": 9,
+    "name": "Home Farm",
+    "sbiNumber": 644322221
+  },
+  {
+    "id": 10,
+    "name": "Inroad Developments Ltd",
+    "sbiNumber": 2001421786
+  },
+  {
+    "id": 11,
+    "name": "Jehan and Wearn Ltd",
+    "sbiNumber": 344211567
+  },
+  {
+    "id": 12,
+    "name": "Kelce Farm",
+    "sbiNumber": 889045441
+  },
+  {
+    "id": 13,
+    "name": "Marley & Daughters",
+    "sbiNumber": 122176668
+  },
+  {
+    "id": 14,
+    "name": "Nigel Thornberry",
+    "sbiNumber": 231109776
+  },
+  {
+    "id": 15,
+    "name": "Oakley Farm",
+    "sbiNumber": 433232111
+  },
+  {
+    "id": 16,
+    "name": "Potter Holdings Ltd",
+    "sbiNumber": 500093456
+  },
+  {
+    "id": 17,
+    "name": "Prime Cuts Farm",
+    "sbiNumber": 500093456
+  },
+  {
+    "id": 18,
+    "name": "RJ Whitfield",
+    "sbiNumber": 433876541
+  },
+  {
+    "id": 19,
+    "name": "Ronnie's Farm",
+    "sbiNumber": 761132198
+  },
+  {
+    "id": 20,
+    "name": "Russer & Mona",
+    "sbiNumber": 121166709
+  },
+  {
+    "id": 21,
+    "name": "Sharon's Farm",
+    "sbiNumber": 489910429
+  },
+  {
+    "id": 22,
+    "name": "Shaw & Carter Ltd",
+    "sbiNumber": 122887431
+  },
+  {
+    "id": 23,
+    "name": "Sunflower Charity",
+    "sbiNumber": 987677432
+  },
+  {
+    "id": 24,
+    "name": "Todmorden Shrimp Farm",
+    "sbiNumber": 121150991
+  },
+  {
+    "id": 25,
+    "name": "Valley Farm",
+    "sbiNumber": 192846125
+  },
+  {
+    "id": 26,
+    "name": "Wright Sisters",
+    "sbiNumber": 123335090
+  }
+ ]
