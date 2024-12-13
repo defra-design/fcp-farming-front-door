d496c916dec05d7bd1276cad838db31443d5a747
Dave Haigh <davehaigh@outlook.com>
2024-12-13 14:09:55 +0000

    added shortDate filter

--- app/filters.js
+++ app/filters.js
@@ -8,3 +8,18 @@ const addFilter = govukPrototypeKit.views.addFilter
 
 // Add your filters here
 
+addFilter('shortDate', function shortDate (x) {
+
+    x = new Date()
+ 
+   const daysOfWeek = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+   const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+ 
+   const dayOfWeek = daysOfWeek[x.getDay()];
+   const dayOfMonth = x.getDate();
+   const month = months[x.getMonth()];
+ 
+   return `${dayOfWeek} ${dayOfMonth} ${month}`;
+ 
+ });
+
