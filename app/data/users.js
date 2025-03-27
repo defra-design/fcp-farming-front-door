3eeeb5d6d8349d11df84794df45bd53c3521013e
Dave Haigh <davehaigh@outlook.com>
2025-03-27 16:21:38 +0000

    v8 updated users data

--- app/data/users.js
+++ app/data/users.js
@@ -7,11 +7,53 @@ module.exports = [
     "crn": "1101996862"
   },
   {
+    "id": 4,
+    "name": "Andrew Fossden",
+    "firstName": "Andrew",
+    "lastName": "Fossden",
+    "crn": "1509995431"
+  },
+  {
     "id": 2,
     "firstName": "Colin",
     "lastName": "Rook",
     "name": "Colin Rook",
     "crn": "1103765975"
+  },
+  {
+      "id": 5,
+      "name": "Dafos Carey",
+      "firstName": "Dafos",
+      "lastName": "Carey",
+      "crn": "1102876541"
+  },
+  {
+      "id": 6,
+      "name": "Foster Smedwick",
+      "firstName": "Foster",
+      "lastName": "Smedwick",
+      "crn": "1106547986"
+  },
+  {
+      "id": 7,
+      "name": "John Foss",
+      "firstName": "John",
+      "lastName": "Foss",
+      "crn": "1104689030"
+  },
+  {
+      "id": 8,
+      "name": "Quentin Astonfoss-Parsley",
+      "firstName": "Quentin",
+      "lastName": "Astonfoss-Parsely",
+      "crn": "1100056783"
+  },
+  {
+      "id": 9,
+      "name": "Rachel Foschini",
+      "firstName": "Rachel",
+      "lastName": "Foschini",
+      "crn": "1109678954"
   },
   {
     "id": 3,
