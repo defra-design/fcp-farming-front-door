b9497f4c735159d31eda2e447b11d361f79c8b76
Dave Haigh <davehaigh@outlook.com>
2025-03-27 12:26:57 +0000

    v8 fake search results for internal

--- app/data/users.js
+++ app/data/users.js
@@ -4,23 +4,20 @@ module.exports = [
     "firstName": "Alfred",
     "lastName": "Waldron",
     "name": "Alfred Waldron",
-    "crn": "1101996862",
-    "email": "alfred.waldron@gmail.com"
+    "crn": "1101996862"
   },
   {
     "id": 2,
     "firstName": "Colin",
     "lastName": "Rook",
     "name": "Colin Rook",
-    "crn": "1103765975",
-    "email": "colin.r@gmail.com"
+    "crn": "1103765975"
   },
   {
     "id": 3,
     "firstName": "Sarah",
     "lastName": "Appleby",
     "name": "Sarah Appleby",
-    "crn": "1105569934",
-    "email": "appleby.sarah@gmail.com"
+    "crn": "1105569934"
   }
 ]
