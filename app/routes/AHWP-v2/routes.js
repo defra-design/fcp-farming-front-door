5b12c3328817eec321db0919b557a4242d3e78bc
annechantpx <anne.chan@tpximpact.com>
2025-10-28 09:18:32 +0000

    IAHW changes post UR/IAHW feedback

--- app/routes/AHWP-v2/routes.js
+++ app/routes/AHWP-v2/routes.js
@@ -1656,21 +1656,26 @@ module.exports = function (router,_myData) {
     });
 
 
-// Run this code when a form is submitted to 'juggling-balls-answer'
-router.post('/detailsroute', function (req, res) {
 
-  // Make a variable and give it the value from 'how-many-balls'
-  var variabledetails = req.session.data['correctdetails']
- 
+//routing for when details are correct/incorrect - prototype 13
+// Run this code when a form is submitted to 'detailsroutev2'
+router.post('/detailsroutev2', function (req, res) {
+
+  // Make a variable and give it the value from 'correctdetailsv2' question
+  var variabledetailsv2 = req.session.data['correctdetailsv2']
+
   // Check whether the variable matches a condition
-  if (variabledetails == "value1"){
+  if (variabledetailsv2 == "value1"){
     // Send user to next page
     res.redirect('AHWP-v2/AHWP-what-you-can-claim')
+   
   } else {
     // Send user to ineligible page
     // ADD PAGE TO GO TO
     res.redirect('AHWP-v2/AHWP-sign-in-details-incorrect')
   }
 });
+
+
 
 }
